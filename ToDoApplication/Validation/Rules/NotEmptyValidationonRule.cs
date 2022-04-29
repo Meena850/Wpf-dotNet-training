285ab34c6c8696f95bf325a634fca4ae9c60f5c9
Meena A <Meena.A@krones.in>
2022-04-29 12:49:25 +0530

    Added Validation

---
+++ ToDoApplication/Validation/Rules/NotEmptyValidationonRule.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Globalization;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Controls;
+
+namespace ToDoApplication.Validation.Rules
+{
+    internal class NotEmptyValidationonRule : ValidationRule
+    {
+        public override ValidationResult Validate(object value, CultureInfo cultureInfo)
+        {
+            if (value is string str)
+            {
+                if (!String.IsNullOrWhiteSpace(str))
+                {
+                    return ValidationResult.ValidResult;
+                }
+                else
+                {
+                    return new ValidationResult(false, "This Field cannot be Empty or WhiteSpace.");
+                }
+            }
+            else
+            {
+                return new ValidationResult(false, "Value must be a string");
+            }
+        }
+    }
+}
