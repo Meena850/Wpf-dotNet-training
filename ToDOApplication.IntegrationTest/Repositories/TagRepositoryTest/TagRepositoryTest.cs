1e42529437cc20a3ac4b51e68178b6f6554aac7a
Meena A <Meena.A@krones.in>
2022-05-20 12:51:52 +0530

    Code Changes

--- ToDOApplication.IntegrationTest/Repositories/TagRepositoryTest/TagRepositoryTest.cs
+++ ToDOApplication.IntegrationTest/Repositories/TagRepositoryTest/TagRepositoryTest.cs
@@ -12,46 +12,108 @@ namespace ToDOApplication.IntegrationTest.Repositories.TagRepositoryTest
     [TestClass]
     public class TagRepositoryTest
     {
+
+        private readonly string  testDir = Path.Combine(Path.GetTempPath(),"TagRepositoryTest");
         [TestMethod]
         public void GetAll_FileContains4Tags_ReturnsListWith4Tags()
         {
             //Arrange
-            var appDataPath = Environment.GetFolderPath(Environment.SpecialFolder.LocalApplicationData);
-            var testDir = Path.Combine(Path.GetTempPath(), "TodoAppTest", Path.GetRandomFileName());
-            if (Directory.Exists(testDir))
-            {
-                Directory.Delete(testDir, true);
-            }
-            Directory.CreateDirectory(testDir);
+            RecreateDirectory(testDir);
+            //Copy test files to directory
+            var tagItemTestFile = CopyFileTotestDir("ReadTagItems.json");
+            var repository = CreateSut(tagItemTestFile);
             //Act
-            try
-            {
-                //Copy test files to directory
-                var SourceFileName = Path.Combine(Environment.CurrentDirectory, "Repositories", "TagRepositoryTest", "Data", "GetAllTagFile.json");
-                var TargetFileName = Path.Combine(testDir, "TagItem.json");
-                File.Copy(SourceFileName, TargetFileName, true);
+            var tags = repository.GetAll();
 
-                var tagItemTestFile = new FileInfo(TargetFileName);
-                var appConfigMock = new Mock<IAppConfigService>();
-                appConfigMock.Setup(s => s.TagItemFile).Returns(tagItemTestFile);
-                
-                var repository = CreateSut(appConfigMock.Object);
-                var tags = repository.GetAll();
-
-
-                tags.Count.ShouldBe(4);
-            }
-            //CleanUp
-            finally
-            {
-                //Remove the Created test dircetory
-                Directory.Delete(testDir, true);
-            }
+            //Assert
+            tags.Count.ShouldBe(4);
         }
-        private ITagRepository CreateSut(IAppConfigService configService)
+
+        [TestMethod]
+        public void Add_FileIsEmpty_TagIsAddedToTheFile()
         {
-            return new TagRepository(configService);
+            //Arrange
+            //Create Test Directory
+            RecreateDirectory(testDir);
+
+            //Copy Test Files to directory
+            var tagItemTestFile = CopyFileTotestDir("EmptyTagFile.json");
+            var appConfigMock = new Mock<IAppConfigService>();
+            appConfigMock.Setup(s => s.TagItemFile).Returns(tagItemTestFile);
+            var repository = CreateSut(tagItemTestFile);
+            //Act
+            repository.Add(CreateTagItem("Tag1"));
+            //Assert
+            File.ReadAllText(tagItemTestFile.FullName).ShouldContain("\"Name\": \"Tag1\"");
         }
+
+        [TestMethod]
+        public void Remove_FileHas2TodoItems_FirstItemIsRemovedFromFile()
+        {
+            //Arrange
+            RecreateDirectory(testDir);
+            var TagItemtestFile = CopyFileTotestDir("GetAllTagFile.json");
+            var repository = CreateSut(TagItemtestFile);
+            //Act
+            repository.Remove(Guid.Parse("ce8146d2-1312-4bf0-a90b-8cd8f0106ac0"));
+
+            //Assert
+            File.ReadAllText(TagItemtestFile.FullName).ShouldNotContain("ce8146d2-1312-4bf0-a90b-8cd8f0106ac0");
+
+        }
+
+
+
+        private ToDoItemTags CreateTagItem(string tagname)
+        {
+            return new ToDoItemTags()
+            {
+                
+                Id = Guid.NewGuid(),
+                Name = tagname
+
+            };
+        }
+
+        [TestCleanup]
+        public void CleanUp()
+        {
+            //CleanUp
+            Directory.Delete(testDir, true);
+
+        }
+        private void RecreateDirectory(string directory)
+        {
+            if (Directory.Exists(directory))
+            {
+                Directory.Delete(directory, true);
+            }
+            Directory.CreateDirectory(directory);
+
+        }
+
+        private FileInfo CopyFileTotestDir( string filename)
+        {
+            var SourceFileName = Path.Combine(Environment.CurrentDirectory, "Repositories", "TagRepositoryTest", "Data", "GetAllTagFile.json");
+            var TargetFileName = Path.Combine(filename);
+            File.Copy(SourceFileName, TargetFileName, true);
+            return new FileInfo(TargetFileName);
+        }
+        private ITagRepository CreateSut(FileInfo filename)
+        {
+            var appconfig=CreateFakeConfigservice(filename);
+            return new TagRepository(appconfig);
+        }
+
+        private IAppConfigService CreateFakeConfigservice(FileInfo tagItemFile)
+        {
+            var appConfigMock = new Mock<IAppConfigService>();
+            appConfigMock.Setup(s => s.TagItemFile).Returns(tagItemFile);
+            return appConfigMock.Object;
+
+        }
+
+
 
     }
 }
--- ToDOApplication.IntegrationTest/Repositories/TodoItemRepositoryTest/Data/TodoItemRepositoryTest.cs
+++ ToDOApplication.IntegrationTest/Repositories/TodoItemRepositoryTest/Data/TodoItemRepositoryTest.cs
@@ -2,7 +2,10 @@
 using Moq;
 using Shouldly;
 using System;
+using System.Collections.Generic;
 using System.IO;
+using System.Linq;
+using ToDoApplication.Model;
 using ToDoApplication.Repositories;
 using ToDoApplication.Services;
 
@@ -11,44 +14,106 @@ namespace ToDOApplication.IntegrationTest.Repositories.TodoItemRepositoryTest.Da
     [TestClass]
     public class TodoItemRepositoryTest
     {
+        private readonly string _testDir = Path.Combine(Path.GetTempPath(), "TodoItemRepositoryTest");
         [TestMethod]
         public void GetAll_FileContainsTwoItems_ReturnsListWIthTwoItems()
         {                
                 //Arrange
                 //Create a unique directory for this test method
-                var appDataPath = Environment.GetFolderPath(Environment.SpecialFolder.LocalApplicationData);
-                var testDir = Path.Combine(Path.GetTempPath(), "TodoAppTest", Path.GetRandomFileName());
-                if (Directory.Exists(testDir))
-                {
-                    Directory.Delete(testDir, true);
-                }
-                Directory.CreateDirectory(testDir);
-            try
-            {
+                ReCreateDirectory(_testDir);
                 //Copy test files to directory
-                var SourceFileName = Path.Combine(Environment.CurrentDirectory, "Repositories", "TodoItemRepositoryTest", "Data", "GetAllTextFile.json");
-                var TargetFileName = Path.Combine(testDir, "GetAllFile.json");
-                File.Copy(SourceFileName, TargetFileName, true);
-
-                var todoItemTestFile = new FileInfo(TargetFileName);
+                var todoItemTestFile = CopyFileTotestDir(_testDir, "GetAllTExtFile.json");
                 var appConfigMock = new Mock<IAppConfigService>();
                 appConfigMock.Setup(s => s.TodoItemFile).Returns(todoItemTestFile);
-                var repository = CreateSut(appConfigMock.Object);
+                var repository = CreateSut(todoItemTestFile);
                 //Act
                 var todos = repository.GetAll();
                 //Assert
-                todos.Count.ShouldBe(1);
-            }
-            //CleanUp
-            finally
-            {
-                //Remove the Created test dircetory
-                Directory.Delete(testDir, true);
-            }
+                todos.Count.ShouldBe(2);
         }
 
-        private ITodoItemRepository CreateSut(IAppConfigService configService)
+        [TestMethod]
+        public void Add_FileIsEmpty_ItemIsAddedToTheFile()
         {
+            //Arrange
+            //Create a unique directory for this test method
+            //Copy test files to directory
+            ReCreateDirectory(_testDir);
+            var todoItemTestFile = CopyFileTotestDir(_testDir, "EmptyTestFile.json");
+            var appConfigMock = new Mock<IAppConfigService>();
+            appConfigMock.Setup(s => s.TodoItemFile).Returns(todoItemTestFile);
+            var repository = CreateSut(todoItemTestFile);
+            //Act
+            repository.Add(CreateToDoitem("Create Integration Tests"));
+            //Assert
+            File.ReadAllText(todoItemTestFile.FullName).ShouldContain("\"Name\": \"Create Integration Tests\"");
+        }
+
+        [TestMethod]
+        public void Remove_FileHas2TodoItems_FirstItemIsRemovedFromFile()
+        {
+            //Arrange
+            ReCreateDirectory(_testDir);
+            var testFile = CopyFileTotestDir(_testDir, "GetAllTextFile.json");
+            var repository = CreateSut(testFile);
+            //Act
+            repository.Remove(Guid.Parse("af774bcb-4bcb-4dfa-b25b-40bedb3fff24"));
+
+            //Assert
+            File.ReadAllText(testFile.FullName).ShouldNotContain("af774bcb-4bcb-4dfa-b25b-40bedb3fff24");
+
+        }
+
+        [TestCleanup]
+        public void CleanUp()
+        {
+            //CleanUp
+            Directory.Delete(_testDir, true);
+
+        }
+
+        private IAppConfigService CreateFakeConfigservice(FileInfo todoItemFile)
+        {
+            var appConfigMock = new Mock<IAppConfigService>();
+            appConfigMock.Setup(s => s.TodoItemFile).Returns(todoItemFile);
+            return appConfigMock.Object;
+
+        }
+
+        private void ReCreateDirectory(String directory)
+        {
+            if (Directory.Exists(directory))
+            {
+                Directory.Delete(directory, true);
+            }
+            Directory.CreateDirectory(directory);
+
+        }
+
+        private FileInfo CopyFileTotestDir(string testDir, string filename)
+        {
+            var SourceFileName = Path.Combine(Environment.CurrentDirectory, "Repositories", "TodoItemRepositoryTest", "Data", filename);
+            var TargetFileName = Path.Combine(testDir, filename);
+            File.Copy(SourceFileName, TargetFileName, true);
+            return new FileInfo(TargetFileName);
+
+        }
+        private ToDoItemModel  CreateToDoitem(string name)
+        {
+            return new ToDoItemModel()
+            {
+                Name = name,
+                Id = Guid.NewGuid(),
+                IsDone = false,
+                TagId = new List<Guid>(),
+                Timestamp = DateTime.Now,
+            };
+
+        }
+
+        private ITodoItemRepository CreateSut(FileInfo testFile)
+        {
+            var configService = CreateFakeConfigservice(testFile);
             return new TodoItemRepository(configService);
         }
     }
